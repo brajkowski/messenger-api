16c87e0243168c72a9ea7dc93b017322bfaccaeb
brajkowski <37646936+brajkowski@users.noreply.github.com>
2021-07-07 22:24:31 -0600

    added WsController (#5)

---
+++ src/ws/ws-controller.ts
@@ -0,0 +1,69 @@
+import http from 'http';
+import WebSocket from 'ws';
+import {
+  IncomingMessage,
+  instanceOfIncomingMessage,
+} from '../messenger/message';
+import { MessengerService } from '../messenger/messenger-service';
+
+export class WsController {
+  static messageHandler(
+    ws: WebSocket,
+    data: WebSocket.Data,
+    messengerService: MessengerService
+  ): void {
+    let incomingMessage: IncomingMessage;
+    try {
+      incomingMessage = JSON.parse(data.toString());
+    } catch (err) {
+      console.debug('[ws-controller] could not parse incoming message');
+      return;
+    }
+    if (!instanceOfIncomingMessage(incomingMessage)) {
+      console.debug('[ws-controller] invalid incoming message');
+      return;
+    }
+    const sender = messengerService.getActiveUsername(ws);
+    if (sender) {
+      messengerService.sendMessage(
+        sender,
+        incomingMessage.recipient,
+        incomingMessage.body
+      );
+    }
+  }
+
+  constructor(private messengerService: MessengerService) {}
+
+  handleNewConnection(ws: WebSocket, req: http.IncomingMessage): void {
+    let url: URL;
+    let path: string;
+    let username: string | null;
+    try {
+      url = new URL(req.url!, 'http://localhost');
+    } catch (err) {
+      console.debug('[ws-controller] could not parse new connection URL');
+      ws.close();
+      return;
+    }
+    path = url.pathname;
+    username = url.searchParams.get('username');
+    if (
+      path.toLocaleLowerCase() !== '/message' ||
+      !username ||
+      username.length < 1
+    ) {
+      console.debug('[ws-controller] incorrect path or missing/empty username');
+      ws.close();
+      return;
+    }
+    this.activateNewConnection(username, ws);
+  }
+
+  protected activateNewConnection(username: string, ws: WebSocket): void {
+    ws.on('message', (data: WebSocket.Data) =>
+      WsController.messageHandler(ws, data, this.messengerService)
+    );
+    this.messengerService.activateUser(username, ws);
+  }
+}
